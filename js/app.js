cea963eb06526a51f902af7d97c609a9668f060b
Cameron Duncan <cameron.duncan@atkinsglobal.com>
2021-01-12 17:02:12 +0000

    build test 03

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/21_01_12_TestScene_test_04.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/21_01_12_TestScene_test_07.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
